196ab04ad69be72a818a247d72048ea33414e327
SIDDHARTH SINGH <singhsiddharth1102@gmail.com>
2023-05-24 04:04:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./components/heading";
+import List from "./components/List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Maggi</li>
+      <li>Maggi</li>
+      <li>Maggi</li>
+    </ul>
+  );
+}
+
+export default List;
